fd6866713c3c39e6ee67ec8cd1506bd0cea72448
Areej Ahmad <ahmadareej244@gmail.com>
2024-08-15 16:57:54 +0530

    folder structure updated 1

--- template.py
+++ template.py
@@ -6,7 +6,7 @@ list_of_files=[
     
     ".github/workflows/.gitkeep",
     "src/__init__.py",
-    "src/components__init__.py",
+    "src/components/__init__.py",
     "src/components/data_ingestion.py",
     "src/components/data_transformation.py",
     "src/components/model_trainer.py",
@@ -14,6 +14,7 @@ list_of_files=[
     "src/pipeline/__init__.py",
     "src/pipeline/training_pipeline.py",
     "src/pipeline/prediction_pipeline.py",
+    "src/utils/__init__.py"
     "src/utils/utils.py",
     "src/logger/logging.py"
     "src/exception/exception.py"
